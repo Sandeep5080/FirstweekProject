7ca2ff9b3c4ff8dcd2921b02d12a6b42ab039179
sreddy370 <sreddy370@US-PG02LYAP>
2021-10-24 20:38:30 +0530

    Control statement

---
+++ FirstweekProject/src/controlstatement/Ifelse2.java
@@ -0,0 +1,19 @@
+package controlstatement;
+
+public class Ifelse2 {
+	public static void main(String args[]) {
+		int n = 7;
+		if (n%2==0)
+		System.out.println("Even");
+		else if (n%2==0)
+		System.out.println("Odd");
+		else
+		System.out.println("odd");
+		
+
+		
+	}
+}
+
+
+
---
+++ FirstweekProject/src/controlstatement/Switch.java
@@ -0,0 +1,26 @@
+package controlstatement;
+
+public class Switch {
+	public static void main(String args[]) {
+		int n = 3;
+		
+		switch(n)
+		{
+		case 1:
+			System.out.println("one");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		case 3:
+			System.out.println("Three");
+			break;
+		case 4:
+			System.out.println("Four");
+		case 5:
+			System.out.println("Five");
+		}
+		
+	}
+
+}
